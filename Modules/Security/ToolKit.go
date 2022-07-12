db1b991b2d60d2f5a992931596030c202cbf706f
Francisco Javier Diaz <pacodiazl@outlook.com>
2022-07-11 19:26:08 -0500

    remove IsSafePassword

--- Modules/Security/ToolKit.go
+++ Modules/Security/ToolKit.go
@@ -3,7 +3,6 @@ package Security
 import (
 	"crypto/rand"
 	"encoding/base64"
-	"unicode"
 )
 
 func generateRandomBytes(n int) ([]byte, error) {
@@ -19,26 +18,4 @@ func generateRandomBytes(n int) ([]byte, error) {
 func GenerateRandomString(s int) (string, error) {
 	b, err := generateRandomBytes(s)
 	return base64.URLEncoding.EncodeToString(b), err
-}
-
-func IsSafePassword(Password string) bool {
-	var Uppercase bool
-	var lowercase bool
-	var Number bool
-	var Digit bool
-
-	for _, v := range Password {
-		if unicode.IsUpper(v) {
-			Uppercase = true
-		}
-		if unicode.IsLower(v) {
-			lowercase = true
-		}
-		if unicode.IsNumber(v) {
-			Number = true
-		}
-
-	}
-
-	return Uppercase && lowercase && Number && Digit
 }
