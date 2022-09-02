8df6779ae52dbcbdff7fbd7d243516055429be48
Francisco Javier Diaz <pacodiazl@outlook.com>
2022-09-01 19:52:38 -0500

    updated error log

--- Modules/Logs/logs.go
+++ Modules/Logs/logs.go
@@ -3,16 +3,17 @@ package Logs
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
 // WriteLogs maneja los errores
-func WriteLogs(errors error) {
+func WriteLogs(errors error, severity int) {
 	s := errors.Error()
 	f, err := os.OpenFile("./error.log",
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
-		makefile("./error.log")
+		makefile(("./error-severity-" + (strconv.Itoa(severity)) + ".log"))
 		panic(err)
 	}
 	defer f.Close()
