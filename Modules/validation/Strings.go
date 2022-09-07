1c966033d2583438943c5717a26cc0989a174cd5
Francisco Javier Diaz <pacodiazl@outlook.com>
2022-09-06 23:10:20 -0500

    add func SliceStringContains

--- Modules/validation/Strings.go
+++ Modules/validation/Strings.go
@@ -6,3 +6,12 @@ func TruncateString(text string, lent int) string {
 	}
 	return text[:lent]
 }
+
+func SliceStringContains(v []string, find any) bool {
+	for _, v2 := range v {
+		if v2 == find {
+			return true
+		}
+	}
+	return false
+}
