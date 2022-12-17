10c97aab984be07d5e059141492eab2b9be9df74
Francisco Javier Diaz <pacodiazl@outlook.com>
2022-12-17 16:12:17 -0600

    Update recaptcha.go

--- extensions/recaptcha/recaptcha.go
+++ extensions/recaptcha/recaptcha.go
@@ -1,19 +1,5 @@
 package recaptcha
 
-import (
-	"errors"
-	"net/http"
-	"os"
-	"strings"
-)
+/*func CheckRecaptcha(ip, response string) (bool, error) {
 
-func CheckRecaptcha(ip, response string) (bool, error) {
-	url := "https://www.google.com/recaptcha/api/siteverify"
-	r := os.Getenv("rSecret")
-	if r == "" {
-		return false, errors.New("not configured recaptcha")
-	}
-	payload := strings.NewReader("secret=" + os.Getenv("rSecret") + "&response=" + response + "&remoteip=" + ip + "")
-	_, _ := http.NewRequest("POST", url, payload)
-	return false, nil
-}
+}*/
