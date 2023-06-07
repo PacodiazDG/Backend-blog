ee4bbaab553b6f5f13c42f301574f005c7cf6b4a
Francisco Javier DLG <pacodiazl@outlook.com>
2023-06-06 22:08:58 -0600

    Update validation.go

--- modules/configinit/validation.go
+++ modules/configinit/validation.go
@@ -7,13 +7,13 @@ import (
 
 func validation() error {
 	if os.Getenv("DB_CONFIG") == "" {
-		return errors.New("DB_CONFIG ")
+		return errors.New("DB_CONFIG cannot be empty")
 	}
 	if os.Getenv("JWT_SECRET") == "" {
-		return errors.New("")
+		return errors.New("JWT_SECRET cannot be empty")
 	}
 	if os.Getenv("TokenExpirationTimeInMinutes") == "" {
-		return errors.New("")
+		return errors.New("TokenExpirationTimeInMinutes cannot be empty")
 	}
 	if os.Getenv("DefaultDatabase") == "" {
 		return errors.New("DefaultDatabase cannot be empty")
