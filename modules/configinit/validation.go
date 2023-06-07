14b6da457f4d499abd28a96086569d1caae84a8f
Francisco Javier DLG <pacodiazl@outlook.com>
2023-06-06 21:51:01 -0600

    Update validation.go

--- modules/configinit/validation.go
+++ modules/configinit/validation.go
@@ -19,62 +19,7 @@ func validation() error {
 		return errors.New("")
 	}
 	if os.Getenv("LogErr") == "" {
-		return errors.New("")
+		return errors.New("LogErr cannot be empty")
 	}
-	// SMTP credencials
-	if os.Getenv("SMTPHost") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("SMTPport") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("SMTPPassword") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("TestSMTP") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("TestEmailSMTP") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("UseSMTP") == "" {
-		return errors.New("")
-	}
-	//Redis credencials
-	if os.Getenv("RedisAddr") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("RedisPassword") == "" {
-		return errors.New("")
-	}
-	//UseProxy
-	if os.Getenv("UseProxy") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("IpaddressByHeader") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("") == "" {
-		return errors.New("")
-	}
-	if os.Getenv("") == "" {
-		return errors.New("")
-	}
-
 	return nil
 }
