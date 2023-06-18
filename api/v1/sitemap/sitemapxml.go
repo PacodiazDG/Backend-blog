6c1355997ae757e9d9b8ff4a91b1c84fd5d4e14d
Francisco Javier DLG <pacodiazl@outlook.com>
2023-06-18 01:14:15 -0600

    fix siteUrl

--- api/v1/sitemap/sitemapxml.go
+++ api/v1/sitemap/sitemapxml.go
@@ -67,7 +67,7 @@ func SiteMapxml(c *gin.Context) {
 	Meta := "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">"
 	for _, value := range results {
 
-		u, err := url.Parse(siteUrl + "/Pages?id=" + value["_id"].(primitive.ObjectID).Hex())
+		u, err := url.Parse(siteUrl + "Pages?id=" + value["_id"].(primitive.ObjectID).Hex())
 		if err != nil {
 			c.AbortWithStatus(http.StatusInternalServerError)
 			return
